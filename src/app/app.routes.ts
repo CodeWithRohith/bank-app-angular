d6aa6bbc43a047e4f9b0d37e28524d84d372fb9d
Rohith Kumar Saravanan <s.rohithkumar1998@gmail.com>
2025-05-17 03:35:36 -0300

    Sprint 1
    Build a responsive form to allow users to create a new account with the following:
    
    Account Name
    
    Initial Balance
    
    Account Type (Chequing/Savings)
    
    A conditionally styled submit button based on account type

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AccountCreateComponent } from './components/account-create/account-create.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'create-account', pathMatch: 'full' },
+  { path: 'create-account', component: AccountCreateComponent }
+];
--- src/app/components/account-create/account-create.component.ts
+++ src/app/components/account-create/account-create.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-account-create',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './account-create.component.html',
   styleUrl: './account-create.component.scss'
 })
-export class AccountCreateComponent {
+export class AccountCreateComponent  implements OnInit {
+
+  accountForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.accountForm = this.fb.group({
+      accountName: ['', [Validators.required, Validators.minLength(3)]],
+      initialBalance: [0, [Validators.required, Validators.min(1)]],
+      accountType: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.accountForm.valid) {
+      console.log('✅ Account Created:', this.accountForm.value);
+    }
+  }
+
+  get accountType() {
+    return this.accountForm.get('accountType')?.value;
+  }
 
 }

8c174cd7b94a8adbfb65bf5b591fd99a2f183ee1
Rohith Kumar Saravanan <s.rohithkumar1998@gmail.com>
2025-05-17 02:57:10 -0300

    Sprint 0

---
+++ src/app/components/fund-transfer/fund-transfer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fund-transfer',
+  imports: [],
+  templateUrl: './fund-transfer.component.html',
+  styleUrl: './fund-transfer.component.scss'
+})
+export class FundTransferComponent {
+
+}
